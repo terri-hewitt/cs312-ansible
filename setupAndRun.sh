f847eb665f527ca05e8ad427bb6a65649b1020a8
Terri Hewitt <hewittt@oregonstate.edu>
2019-05-18 15:05:54 -0700

    Fixed file location

--- setupAndRun.sh
+++ setupAndRun.sh
@@ -3,10 +3,10 @@
 yum install -y ansible
 
 ssh-keygen -t rsa
-ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.20
-ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.21
-ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.22
-ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.23
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.20
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
 
 ansible-playbook webserver.yaml ./hosts.ini
 curl 192.168.1.20
