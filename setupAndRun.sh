80521d7891f11ce21275bcfae6fd84037fceeb70
Terri Hewitt <hewittt@oregonstate.edu>
2019-05-18 15:09:58 -0700

    Added -i to ansible command

--- setupAndRun.sh
+++ setupAndRun.sh
@@ -8,7 +8,7 @@ ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
 ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
 ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
 
-ansible-playbook webserver.yaml ./hosts.ini
+ansible-playbook webserver.yaml -i ./hosts.ini
 curl 192.168.1.20
 curl 192.168.1.21
 curl 192.168.1.22
