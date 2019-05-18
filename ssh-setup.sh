0a97379d2bb612a613ab6c43250b148219718492
Terri Hewitt <hewittt@oregonstate.edu>
2019-05-18 14:11:28 -0700

    Added ansible commands and curl

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 ssh-keygen -t rsa
-echo -ne '\n'
-echo -ne '\n'
-echo -ne '\n'
 ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.22
-echo 'yes'
-echo 'password'
 ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.23
-echo 'yes'
-echo 'password'
 ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.24
-echo 'yes'
-echo 'password'
 ssh-copy-id -i ~/ssh/id_rsa root@192.168.1.25
-echo 'yes'
-echo 'password'
+
+ansible-playbook webserver.yaml ./hosts.ini
+curl 192.168.1.22
+curl 192.168.1.23
+curl 192.168.1.24
+curl 192.168.1.25
